e0ccf57c2383230986b065ff63bc253e1d6a5ac2
Matthew Britt <mwbritt@hotmail.com>
2019-12-31 00:43:59 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,6 +12,7 @@ pretty_print_nds(directors_database)
 def print_first_directors_movie_titles
   row_index = 0 
   movies = []
-  while row_index < directors_database.count do
+  while directors_database[0] do
+    if 
     
 end
